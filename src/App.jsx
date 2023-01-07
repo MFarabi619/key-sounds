9dcc6de35904578f0b39881cff0474d7e959796e
Mumtahin Farabi <mfarabi619@gmail.com>
2023-01-07 18:45:18 -0500

    Placed all 'drum-pad' elements outside of 'display' container

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ function App() {
     <div className="App">
       <container id="drum-machine">
         <container id="display">
+          Some text to be displayed here
+        </container>
+
           <button className="drum-pad" id="Heater 1"><audio className="clip" id="W" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"></audio> W</button>
           <button className="drum-pad" id="Heater 2"><audio className="clip" id="Q" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"></audio> Q</button>
           <button className="drum-pad" id="Heater 3"><audio className="clip" id="E" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"></audio> E</button>
@@ -16,7 +19,8 @@ function App() {
           <button className="drum-pad" id="Kick-n'-Hat"><audio className="clip" id="Z" src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"></audio> Z</button>
           <button className="drum-pad" id="Kick"><audio className="clip" id="X" src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"></audio> X</button>
           <button className="drum-pad" id="Closed-HH"><audio className="clip" id="C" src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"></audio> C</button>
-        </container></container>    
+      
+      </container>    
     </div>
   )
 }
