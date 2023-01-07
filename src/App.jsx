a3c428ac80f0942d77bfa87d9e270bc9d413f9e7
Mumtahin Farabi <mfarabi619@gmail.com>
2023-01-07 17:33:28 -0500

    Created container structure for keys

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,18 @@ function App() {
 
   return (
     <div className="App">
-<div>some text</div>    
+      <container id="drum-machine">
+        <container id="display">
+          <div className="drump-pad">Q</div>
+          <div className="drump-pad">W</div>
+          <div className="drump-pad">E</div>
+          <div className="drump-pad">A</div>
+          <div className="drump-pad">S</div>
+          <div className="drump-pad">D</div>
+          <div className="drump-pad">Z</div>
+          <div className="drump-pad">X</div>
+          <div className="drump-pad">C</div>
+        </container></container>    
     </div>
   )
 }
