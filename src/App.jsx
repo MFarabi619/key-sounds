b5dacb4b9e2d835dfbd1ca8860cd914615bcda26
Mumtahin Farabi <mfarabi619@gmail.com>
2023-01-07 18:43:12 -0500

    Changed 'drum-pad' elements from divs to buttons

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,15 @@ function App() {
     <div className="App">
       <container id="drum-machine">
         <container id="display">
-          <div className="drum-pad" id="Heater 1"><audio className="clip" id="W" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"></audio> W</div>
-          <div className="drum-pad" id="Heater 2"><audio className="clip" id="Q" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"></audio> Q</div>
-          <div className="drum-pad" id="Heater 3"><audio className="clip" id="E" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"></audio> E</div>
-          <div className="drum-pad" id="Heater 4"><audio className="clip" id="A" src=""></audio> A</div>
-          <div className="drum-pad" id="Clap"><audio className="clip" id="S" src=""></audio> S</div>
-          <div className="drum-pad" id="Open-HH"><audio className="clip" id="D" src=""></audio> D</div>
-          <div className="drum-pad" id="Kick-n'-Hat"><audio className="clip" id="Z" src=""></audio> Z</div>
-          <div className="drum-pad" id="Kick"><audio className="clip" id="X" src=""></audio> X</div>
-          <div className="drum-pad" id="Closed-HH"><audio className="clip" id="C" src=""></audio> C</div>
+          <button className="drum-pad" id="Heater 1"><audio className="clip" id="W" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"></audio> W</button>
+          <button className="drum-pad" id="Heater 2"><audio className="clip" id="Q" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"></audio> Q</button>
+          <button className="drum-pad" id="Heater 3"><audio className="clip" id="E" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"></audio> E</button>
+          <button className="drum-pad" id="Heater 4"><audio className="clip" id="A" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"></audio> A</button>
+          <button className="drum-pad" id="Clap"><audio className="clip" id="S" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"></audio> S</button>
+          <button className="drum-pad" id="Open-HH"><audio className="clip" id="D" src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"></audio> D</button>
+          <button className="drum-pad" id="Kick-n'-Hat"><audio className="clip" id="Z" src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"></audio> Z</button>
+          <button className="drum-pad" id="Kick"><audio className="clip" id="X" src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"></audio> X</button>
+          <button className="drum-pad" id="Closed-HH"><audio className="clip" id="C" src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"></audio> C</button>
         </container></container>    
     </div>
   )
