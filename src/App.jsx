8a49c0decebaba21a943134a7eb5ac6f7ee7a65d
Mumtahin Farabi <mfarabi619@gmail.com>
2023-01-07 17:57:14 -0500

    Added id's to each key corresponding to sound that will be played

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,15 @@ function App() {
     <div className="App">
       <container id="drum-machine">
         <container id="display">
-          <div className="drum-pad">Q</div>
-          <div className="drum-pad">W</div>
-          <div className="drum-pad">E</div>
-          <div className="drum-pad">A</div>
-          <div className="drum-pad">S</div>
-          <div className="drum-pad">D</div>
-          <div className="drum-pad">Z</div>
-          <div className="drum-pad">X</div>
-          <div className="drum-pad">C</div>
+          <div className="drum-pad" id="Heater 1">W</div>
+          <div className="drum-pad" id="Heater 2">Q</div>
+          <div className="drum-pad" id="Heater 3">E</div>
+          <div className="drum-pad" id="Heater 4">A</div>
+          <div className="drum-pad" id="Clap">S</div>
+          <div className="drum-pad" id="Open-HH">D</div>
+          <div className="drum-pad" id="Kick-n'-Hat">Z</div>
+          <div className="drum-pad" id="Kick">X</div>
+          <div className="drum-pad" id="Closed-HH">C</div>
         </container></container>    
     </div>
   )
